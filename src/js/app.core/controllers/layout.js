9ac50f666afdd3f72a34ee35e6db7831f7d5d860
Brit Butler <redline6561@gmail.com>
2016-11-23 11:45:25 -0500

    Create an Add Product link in the layout.

--- src/js/app.core/controllers/layout.js
+++ src/js/app.core/controllers/layout.js
@@ -1,16 +1,19 @@
 function LayoutController ($rootScope, UserService) {
   let vm = this;
 
+  vm.admin = UserService.isAdmin();
   vm.loggedIn = UserService.isLoggedIn();
   vm.logout = logout;
 
   $rootScope.$on('loginChange', (event, data) => {
     vm.loggedIn = UserService.isLoggedIn();
+    vm.admin = UserService.isAdmin();
   });
 
   function logout () {
     UserService.logout();
     vm.loggedIn = false;
+    vm.admin = false;
   }
 
 };
